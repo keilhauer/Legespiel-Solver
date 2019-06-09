4754d5f16aa48c9b5210f475a2a20a682797daf3
Andreas Keilhauer <andreas@keilhauer.eu>
2019-06-09 04:11:44 +0200

    Added new Legespiel "Die Schweineknobelei"

---
+++ src/main/java/org/whatsoftwarecando/legespiel/configs/DieSchweineknobeleiConfig.java
@@ -0,0 +1,65 @@
+package org.whatsoftwarecando.legespiel.configs;
+
+import java.util.ArrayList;
+
+import org.whatsoftwarecando.legespiel.Card;
+import org.whatsoftwarecando.legespiel.Field;
+import org.whatsoftwarecando.legespiel.GameConfig;
+import org.whatsoftwarecando.legespiel.IPicture;
+
+/**
+ * Configuration based on "Die Schweineknobelei". Find more information here:
+ * https://slideplayer.org/slide/666364/
+ */
+public class DieSchweineknobeleiConfig extends GameConfig {
+
+	enum Picture implements IPicture {
+
+		PINK_UP(1), PINK_LOW(1), STRIPES_UP(2), STRIPES_LOW(2), BLACK_SPOTS_UP(3), BLACK_SPOTS_LOW(3), BLUE_DROPS_UP(
+				4), BLUE_DROPS_DOWN(4);
+
+		private final int pairNumber;
+
+		private Picture(int pairNumber) {
+			this.pairNumber = pairNumber;
+		}
+
+		@Override
+		public boolean matches(IPicture other) {
+			if (other instanceof Picture) {
+				return this.pairNumber == ((Picture) other).pairNumber && this != other;
+			} else {
+				return false;
+			}
+
+		}
+	}
+
+	@Override
+	protected Field createEmptyField() {
+		return new Field(3, 3);
+	}
+
+	@Override
+	protected ArrayList<Card> createAvailableCards() {
+		ArrayList<Card> availableCards = new ArrayList<Card>();
+
+		// Identical cards: 2nd, 9th; 3rd, 5th, 6th, 8th; 4th, 7th
+		availableCards.add(new Card(Picture.PINK_UP, Picture.STRIPES_UP, Picture.STRIPES_UP, Picture.PINK_UP));
+		availableCards
+				.add(new Card(Picture.BLUE_DROPS_UP, Picture.STRIPES_LOW, Picture.BLACK_SPOTS_UP, Picture.PINK_LOW));
+		availableCards.add(new Card(Picture.PINK_UP, Picture.BLACK_SPOTS_LOW, Picture.STRIPES_UP, Picture.PINK_UP));
+		availableCards
+				.add(new Card(Picture.PINK_LOW, Picture.BLACK_SPOTS_LOW, Picture.BLACK_SPOTS_UP, Picture.STRIPES_UP));
+		availableCards.add(new Card(Picture.PINK_UP, Picture.BLACK_SPOTS_LOW, Picture.STRIPES_UP, Picture.PINK_UP));
+		availableCards
+				.add(new Card(Picture.PINK_LOW, Picture.STRIPES_LOW, Picture.STRIPES_UP, Picture.BLUE_DROPS_DOWN));
+		availableCards.add(
+				new Card(Picture.STRIPES_LOW, Picture.BLUE_DROPS_DOWN, Picture.BLUE_DROPS_UP, Picture.BLACK_SPOTS_LOW));
+		availableCards
+				.add(new Card(Picture.PINK_LOW, Picture.BLUE_DROPS_DOWN, Picture.BLACK_SPOTS_UP, Picture.STRIPES_LOW));
+		availableCards
+				.add(new Card(Picture.BLUE_DROPS_UP, Picture.BLACK_SPOTS_LOW, Picture.PINK_UP, Picture.STRIPES_LOW));
+		return availableCards;
+	}
+}
