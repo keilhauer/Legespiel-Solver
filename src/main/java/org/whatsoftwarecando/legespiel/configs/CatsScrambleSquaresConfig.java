f424847a5eeac20a4a453e171c8614bb170137d4
Andreas Keilhauer <andreas@keilhauer.eu>
2019-06-15 00:08:15 +0200

    Added Cats Scramble Squares

---
+++ src/main/java/org/whatsoftwarecando/legespiel/configs/CatsScrambleSquaresConfig.java
@@ -0,0 +1,64 @@
+package org.whatsoftwarecando.legespiel.configs;
+
+import java.util.ArrayList;
+
+import org.whatsoftwarecando.legespiel.Card;
+import org.whatsoftwarecando.legespiel.Field;
+import org.whatsoftwarecando.legespiel.GameConfig;
+import org.whatsoftwarecando.legespiel.IPicture;
+
+/**
+ * see https://www.youtube.com/watch?v=Mux4NpO_fsA
+ */
+public class CatsScrambleSquaresConfig extends GameConfig {
+
+	enum Picture implements IPicture {
+
+		WHITE_GRAY_UP(1), WHITE_GRAY_LOW(1), BROWN_UP(2), BROWN_LOW(2), WHITE_RED_UP(3), WHITE_RED_LOW(3), BLACK_LEFT(
+				4), BLACK_RIGHT(4);
+
+		private final int pairNumber;
+
+		private Picture(int pairNumber) {
+			this.pairNumber = pairNumber;
+		}
+
+		@Override
+		public boolean matches(IPicture other) {
+			if (other instanceof Picture) {
+				return this.pairNumber == ((Picture) other).pairNumber && this != other;
+			} else {
+				return false;
+			}
+
+		}
+	}
+
+	@Override
+	protected ArrayList<Card> createAvailableCards() {
+		ArrayList<Card> availableCards = new ArrayList<Card>();
+		availableCards
+				.add(new Card(Picture.WHITE_RED_UP, Picture.BROWN_UP, Picture.WHITE_GRAY_LOW, Picture.BLACK_LEFT));
+		availableCards
+				.add(new Card(Picture.WHITE_GRAY_LOW, Picture.BROWN_LOW, Picture.BLACK_LEFT, Picture.WHITE_RED_UP));
+		availableCards
+				.add(new Card(Picture.BLACK_RIGHT, Picture.WHITE_RED_UP, Picture.BROWN_UP, Picture.WHITE_GRAY_UP));
+		availableCards
+				.add(new Card(Picture.BROWN_UP, Picture.WHITE_RED_UP, Picture.WHITE_GRAY_LOW, Picture.WHITE_GRAY_UP));
+		availableCards.add(new Card(Picture.BROWN_LOW, Picture.WHITE_RED_LOW, Picture.BLACK_LEFT, Picture.BROWN_LOW));
+		availableCards
+				.add(new Card(Picture.WHITE_GRAY_UP, Picture.BROWN_LOW, Picture.WHITE_RED_LOW, Picture.BLACK_LEFT));
+		availableCards
+				.add(new Card(Picture.WHITE_RED_UP, Picture.BLACK_RIGHT, Picture.BLACK_RIGHT, Picture.WHITE_GRAY_UP));
+		availableCards
+				.add(new Card(Picture.BLACK_LEFT, Picture.BROWN_LOW, Picture.WHITE_RED_LOW, Picture.WHITE_GRAY_LOW));
+		availableCards
+				.add(new Card(Picture.WHITE_GRAY_LOW, Picture.WHITE_RED_LOW, Picture.BROWN_UP, Picture.BLACK_RIGHT));
+		return availableCards;
+	}
+
+	@Override
+	protected Field createEmptyField() {
+		return new Field(3, 3);
+	}
+}
