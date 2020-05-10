f1891d65b9ce3510a710ef47f6923452853e76ec
Andreas Keilhauer <github@keilhauer.eu>
2020-05-10 23:15:18 +0200

    Added new game Vino Vino Vino Wine

---
+++ src/main/java/org/whatsoftwarecando/legespiel/configs/VinoVinoVinoWineConfig.java
@@ -0,0 +1,55 @@
+package org.whatsoftwarecando.legespiel.configs;
+
+import org.whatsoftwarecando.legespiel.Field;
+import org.whatsoftwarecando.legespiel.GameConfig;
+import org.whatsoftwarecando.legespiel.IPicture;
+
+/**
+ * Configuration based on "Vino Vino Vino Wine" (see
+ * https://www.bonanza.com/items/like/209090358/Vin-Vino-Wein-Wine-Scramble-Squares-Puzzle
+ * )
+ */
+public class VinoVinoVinoWineConfig extends GameConfig {
+
+	enum Picture implements IPicture {
+
+		BARREL_UPPER(1), BARREL_LOWER(1), TREE_UPPER(2), TREE_LOWER(2), GREEN_VINE_LEFT(3), GREEN_VINE_RIGHT(
+				3), BLUE_VINE_LEFT(4), BLUE_VINE_RIGHT(4);
+
+		private final int pairNumber;
+
+		private Picture(int pairNumber) {
+			this.pairNumber = pairNumber;
+		}
+
+		@Override
+		public boolean matches(IPicture other) {
+			if (other instanceof Picture) {
+				return this.pairNumber == ((Picture) other).pairNumber && this != other;
+			} else {
+				return false;
+			}
+
+		}
+	}
+
+	@Override
+	protected void createAvailableCards() {
+
+		addCard(Picture.GREEN_VINE_RIGHT, Picture.BARREL_UPPER, Picture.TREE_UPPER, Picture.BLUE_VINE_RIGHT);
+		addCard(Picture.BLUE_VINE_RIGHT, Picture.TREE_UPPER, Picture.BARREL_UPPER, Picture.GREEN_VINE_LEFT);
+		addCard(Picture.BARREL_UPPER, Picture.TREE_UPPER, Picture.GREEN_VINE_LEFT, Picture.BLUE_VINE_RIGHT);
+		addCard(Picture.BARREL_LOWER, Picture.TREE_UPPER, Picture.TREE_LOWER, Picture.BLUE_VINE_RIGHT);
+		addCard(Picture.BARREL_UPPER, Picture.TREE_LOWER, Picture.GREEN_VINE_RIGHT, Picture.BLUE_VINE_LEFT);
+		addCard(Picture.BLUE_VINE_LEFT, Picture.GREEN_VINE_RIGHT, Picture.TREE_UPPER, Picture.BARREL_UPPER);
+		addCard(Picture.BLUE_VINE_LEFT, Picture.GREEN_VINE_RIGHT, Picture.GREEN_VINE_LEFT, Picture.BARREL_UPPER);
+		addCard(Picture.BLUE_VINE_RIGHT, Picture.GREEN_VINE_RIGHT, Picture.TREE_LOWER, Picture.BARREL_LOWER);
+		addCard(Picture.BARREL_LOWER, Picture.TREE_UPPER, Picture.GREEN_VINE_RIGHT, Picture.BLUE_VINE_RIGHT);
+
+	}
+
+	@Override
+	protected Field createEmptyField() {
+		return new Field(3, 3);
+	}
+}
